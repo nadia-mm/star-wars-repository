42379c778fcb8d6c3aef65848b8e3e7091cbc93a
Nadia Monayong <nadia.monayongmedjo@gmail.com>
2022-02-11 00:14:19 +0100

    fix public folder url

--- src/Constant.js
+++ src/Constant.js
@@ -1,4 +1,5 @@
+const { PUBLIC_URL } = process.env;
 export const PEOPLE_URL = 'https://swapi.dev/api/people';
 export const STARSHIP_URL = 'https://swapi.dev/api/starships';
 export const PLANET_URL = 'https://swapi.dev/api/planets';
-export const HOME_IMAGE_URL = `${process.env.PUBLIC_URL}/img/pexels-anna-tarazevich-5939140.jpg`;
+export const HOME_IMAGE_URL = `${PUBLIC_URL}/img/pexels-anna-tarazevich-5939140.jpg`;
