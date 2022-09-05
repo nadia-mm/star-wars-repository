7474e8de7f3394a10a3e76eb9508d5440267730e
Nadia MM <44876058+Nadieva@users.noreply.github.com>
2022-09-06 01:37:26 +0200

    simplify the menu items and fix hamburger style

--- src/Constant.js
+++ src/Constant.js
@@ -3,3 +3,4 @@ export const PEOPLE_URL = 'https://swapi.dev/api/people';
 export const STARSHIP_URL = 'https://swapi.dev/api/starships';
 export const PLANET_URL = 'https://swapi.dev/api/planets';
 export const HOME_IMAGE_URL = `${PUBLIC_URL}/img/pexels-anna-tarazevich-5939140.jpg`;
+export const menus = ['Home', 'People', 'Planets', 'Starships', 'Films'];
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,56 +1,37 @@
 import React from 'react';
 import './navbar.scss';
 import Hamburger from 'hamburger-react';
+import { menus } from '../Constant';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Navbar = () => (
   <>
-    <div className="navbar">
-      <a href="/home" className="menu_item">
-        Home
-      </a>
-      <a href="/people" className="menu_item">
-        People
-      </a>
-      <a href="/planets" className="menu_item">
-        Planets
-      </a>
-      <a href="/starships" className="menu_item">
-        Starships
-      </a>
-      <a href="/films" className="menu_item">
-        Films
-      </a>
-    </div>
+    <div className="navbar">{menus.map((menu) => createMenuItem(menu))}</div>
     <div className="navbar navbar__mobile">
       <Hamburger
         onToggle={(toggled) => {
           const menuList = document.getElementsByClassName('burgerMenu');
           const content = document.getElementsByClassName('content');
+
           if (toggled) {
             menuList[0].style.display = 'block';
-            content[0].style.marginTop = '100px';
+            content[0].style.top = '330px';
           } else {
             menuList[0].style.display = 'none';
-            content[0].style.marginTop = '30px';
+            content[0].style.top = '40px';
           }
         }}
       />
 
       <div className="burgerMenu">
-        <a href="/people" className="menu_item">
-          People
-        </a>
-        <a href="/planets" className="menu_item">
-          Planets
-        </a>
-        <a href="/starships" className="menu_item">
-          Starships
-        </a>
-        <a href="/films" className="menu_item">
-          Films
-        </a>
+        {menus.map((menu) => createMenuItem(menu))}
       </div>
     </div>
   </>
+);
+
+const createMenuItem = (title) => (
+  <a href={`/${title.toLowerCase()}`} className="menu_item">
+    {title}
+  </a>
 );
